a3968320407dd6852df6589328364a5188e0b1dd
OgyTirta <o.tirta72@gmail.com>
2019-07-05 12:32:16 +0800

    First

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>From</title>
+</head>
+<body>
+    <h3>Input Data Peserta</h3>
+    <form action="">
+        <label for="id">Id Peserta : </label>
+            <input type="text" name="id" id="id_p" size="16%"><br><br>
+        <label for="nama">Nama : </label>
+            <input type="text" name="nama" id="nama"><br><br>
+        <label for="email">Email : </label>
+            <input type="email" name="email" id="email"><br><br>
+        <input type="submit" value="Simpan">
+    </form>
+</body>
+</html>
---
+++ table.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tabel</title>
+</head>
+<body>
+
+    <table border="1px" cellspacing="1" align="center" width="50%">
+        <caption>Datar Peserta</caption>
+            <thead>
+                <tr>
+                    <th>Id Peserta</th>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody align="center">
+                <tr>
+                    <td>001</td>
+                    <td>Budi</td>
+                    <td>Samarinda</td>
+                    <td>budi@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>002</td>
+                    <td>Jason</td>
+                    <td>Samarinda</td>
+                    <td>jason@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>003</td>
+                    <td>Bunga</td>
+                    <td>Samarinda</td>
+                    <td>bunga@gmail.com</td>
+                </tr>
+            </tbody>
+    </table>
+    
+</body>
+</html>
