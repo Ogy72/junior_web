ae77846f7ade09f6ab70ccfa7601015273874a62
OgyTirta <o.tirta72@gmail.com>
2019-07-08 12:02:24 +0800

    Belajar Grid System

--- index.php
+++ index.php
@@ -8,10 +8,30 @@
 
     <!-- link css -->
     <link rel="stylesheet" href="css/bootstrap.css">    
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="container">
         <h1>Belajar Bootstrap</h1>
+        <div class="row">
+            <div class="col">Col-1</div>
+            <div class="col">Col-2</div>
+            <div class="col">Col-3</div>
+            <div class="col-4">Col-4</div>
+        </div>
+        <div class="row">
+            <div class="col-4">Col-1</div>
+            <div class="col">Col-2</div>
+            <div class="col">Col-3</div>
+        </div>
+        <div class="row">
+            <div class="col">Col-1</div>
+            <div class="col">Col-2</div>
+        </div>
+        <div class="row">
+            <div class="col">Col-1</div>
+        </div>
     </div>
 
     <!-- link js -->
@@ -19,4 +39,5 @@
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.mim.js"></script>
 </body>
+
 </html>
