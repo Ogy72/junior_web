8c05803ceab15da6fe7392e1fef61df6400466b1
OgyTirta <o.tirta72@gmail.com>
2019-07-08 09:49:25 +0800

    use bootstarp

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Junior Web</title>
+
+    <!-- link css -->
+    <link rel="stylesheet" href="css/bootstrap.css">    
+</head>
+<body>
+    <div class="container">
+        <h1>Belajar Bootstrap</h1>
+    </div>
+
+    <!-- link js -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.mim.js"></script>
+</body>
+</html>
