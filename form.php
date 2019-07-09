77022900780e5f7df59e718db898134977790e1a
OgyTirta <o.tirta72@gmail.com>
2019-07-09 12:31:54 +0800

    set layout form

--- form.php
+++ form.php
@@ -5,17 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>From</title>
+
+    <!-- link css -->
+    <link rel="stylesheet" href="css/bootstrap.css">    
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h3>Input Data Peserta</h3>
-    <form action="">
-        <label for="id">Id Peserta : </label>
-            <input type="text" name="id" id="id_p" size="16%"><br><br>
-        <label for="nama">Nama : </label>
-            <input type="text" name="nama" id="nama"><br><br>
-        <label for="email">Email : </label>
-            <input type="email" name="email" id="email"><br><br>
-        <input type="submit" value="Simpan">
-    </form>
+    
+    <div class="container">
+        <div class=row>
+            <div class="col-8">
+            <h3>Input Data Peserta</h3>
+            <br>
+                <form action="">
+                    <div class="form-group">
+                        <div class="form-row">
+                            <div class="form-group col-6">
+                                <label for="nama">Nama</label>
+                                <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukkan Nama">
+                            </div>
+                            <div class="form-group col-6">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Masukkan Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="password" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" name="pass" id="pass" class="form-control" placeholder="Masukkan Password">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="Tanggal" class="col-sm-2 col-form-label">Tanggal</label>
+                            <div class="col-sm-10">
+                                <input type="date" name="date" id="date" class="form-control" placeholder="Pilih Tanggal">
+                            </div>
+                        </div>
+                        <label for="Komentar">Komentar</label>
+                        <textarea class="form-control" name="komentar" id="komentar" rows="3" placeholder="isi Komentar"></textarea>
+                    </div>                
+                    <input type="submit" value="Simpan" class="btn btn-primary" style="width:100%">
+                </form> 
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -12,25 +12,54 @@
 </head>
 
 <body>
-    <div class="container">
-        <h1>Belajar Bootstrap</h1>
+    <div class="container" style="text-align:center; color:#ffffff;">
+        <h1 align="center" style="color:#000000">Belajar Bootstrap</h1>
+
+        <!-- Navigasi -->
         <div class="row">
-            <div class="col">Col-1</div>
-            <div class="col">Col-2</div>
-            <div class="col">Col-3</div>
-            <div class="col-4">Col-4</div>
+            <div class="col-12 col-sm-12 col-md-12 col-xl-12 col">
+                Tab Navigasi
+            </div>
         </div>
+
+        <!-- Header -->
         <div class="row">
-            <div class="col-4">Col-1</div>
-            <div class="col">Col-2</div>
-            <div class="col">Col-3</div>
+            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 col" style="height:50px">
+                ini Logo
+            </div>
+            <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 col">
+                ini Header
+            </div>
         </div>
+
+        <!-- Konten -->
         <div class="row">
-            <div class="col">Col-1</div>
-            <div class="col">Col-2</div>
+            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sidebar" >
+                ini Sidebar
+            </div>
+            <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 col konten">
+
+                <!-- isi Konten -->
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 top-konten" >
+                        ini konten
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 bottom-konten">
+                        ini konten 1
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 bottom-konten">
+                        ini konten 2
+                    </div>
+                </div>
+
+            </div>
         </div>
+
+        <!-- Footer -->
         <div class="row">
-            <div class="col">Col-1</div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col">
+                ini Footer
+            </div>
         </div>
     </div>
 
